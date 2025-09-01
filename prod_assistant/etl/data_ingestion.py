b9b38f0ae713793ba02fcd9991a741621041b3ce
bhavjeet <bhavjeetsingh784@gmail.com>
2025-09-02 00:51:03 +0530

    .

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -8,14 +8,18 @@ from prod_assistant.utils.model_loader import ModelLoader
 from prod_assistant.utils.config_loader import load_config
 
 class DataIngestion:
-    'class to handle data transformation and ingestion into AstraDB vector store'
+    """
+    Class to handle data transformation and ingestion into AstraDB vector store.
+    """
 
     def __init__(self):
-        'Initialize environment variables, embedding model, and set CSV file path'
-        print('Initializing DataIngestion pipeline...')
+        """
+        Initialize environment variables, embedding model, and set CSV file path.
+        """
+        print("Initializing DataIngestion pipeline...")
         self.model_loader=ModelLoader()
-        self._load_env_variable()
-        self.csv_path 
+        self._load_env_variables()
+        self.csv_path = self._get_csv_path()
         self.product_data = self._load_csv()
         self.config=load_config()
 
