c29ca903a5d893a761456e98a287ddf779df43e5
bhavjeet <bhavjeetsingh784@gmail.com>
2025-09-02 00:25:51 +0530

    error fixed

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -5,7 +5,7 @@ from typing import List
 from langchain_core.documents import Document
 from langchain_astradb import AstraDBVectorStore
 from prod_assistant.utils.model_loader import ModelLoader
-from product_assistant.utils.config_loader import load_config
+from prod_assistant.utils.config_loader import load_config
 
 class DataIngestion:
     'class to handle data transformation and ingestion into AstraDB vector store'
