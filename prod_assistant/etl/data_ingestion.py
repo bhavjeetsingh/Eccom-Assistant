a23b1c52e0801c7d5f350d880b68e12c24250280
bhavjeet <bhavjeetsingh784@gmail.com>
2025-09-27 14:17:12 +0530

    data ingestion and test file updated

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -110,7 +110,7 @@ class DataIngestion:
             token=self.db_application_token,
             namespace=self.db_keyspace,
         )
-
+        
         inserted_ids = vstore.add_documents(documents)
         print(f"Successfully inserted {len(inserted_ids)} documents into AstraDB.")
         return vstore, inserted_ids
